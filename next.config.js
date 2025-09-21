1bc549b7af4e01f1411978b5c0fed603bfb23248
jordaneaster <jordaneaster51@gmail.com>
2025-09-21 18:44:32 -0400

    storage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['rzdoygryvifvcmhhbiaq.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rzdoygryvifvcmhhbiaq.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
